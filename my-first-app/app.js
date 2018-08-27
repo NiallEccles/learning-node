786aee8fe849ae52aa06194e7aaca718e20b5be9
Niall Eccles <n.eccles14@gmail.com>
2018-08-27 20:11:30 +0100

    Added in first app.

---
+++ my-first-app/app.js
@@ -0,0 +1,8 @@
+//Usfule for when exporting an object
+// const logger  = require('./logger');
+
+// logger.log('Hello World');
+
+const log = require('./logger');
+
+log('Hello World');
---
+++ my-first-app/logger.js
@@ -0,0 +1,12 @@
+var url = 'http://example.com/';
+
+function log(message){
+    //Send HTTP request
+    console.log(message);
+}
+
+// This will export an object
+// module.exports.log = log;
+
+//This will just export the function
+module.exports = log;
