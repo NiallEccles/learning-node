de22718b28e3a4cca11483032e618b8d55adefe2
Niall Eccles <nialle@rothco.ie>
2018-08-30 13:39:15 +0100

    Logging the current memory and free memory of the system.

--- my-first-app/app.js
+++ my-first-app/app.js
@@ -6,3 +6,21 @@
 const log = require('./logger');
 
 log('Hello World');
+
+
+//Logging the memory of the device
+
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+//Logging the current files in directory
+
+const fs =  require('fs');
+const files = fs.readdirSync('./');
+
+console.log(files);
