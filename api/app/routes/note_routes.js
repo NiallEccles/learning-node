ff60b37c56b7bb8493870c31b03540753de6759b
Niall Eccles <n.eccles14@gmail.com>
2018-09-02 12:09:19 +0100

    Added in ability to view note in browser.

--- api/app/routes/note_routes.js
+++ api/app/routes/note_routes.js
@@ -1,4 +1,16 @@
+var ObjectID = require('mongodb').ObjectId
+
 module.exports = function(app, db){
+    app.get('/notes/:id', (req, res)=>{
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.db().collection('notes').findOne(details, (err, item)=>{
+            if (err) res.send({ 'error': 'An error has occured' });
+            else {
+                res.send(item);
+            }
+        });
+    })
     app.post('/notes', (req, res)=>{
         //Create note
         const note =  { text: req.body.body, title: req.body.title }
